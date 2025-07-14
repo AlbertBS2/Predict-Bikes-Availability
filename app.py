6f140ee960e0a41a4f02a855adc9663819968317
abs2 <albertbargallosales@gmail.com>
2025-07-14 02:51:07 +0200

    Feat: first predictions in app

--- app.py
+++ app.py
@@ -3,75 +3,111 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import datetime
-import calendar
-from sklearn.ensemble import RandomForestClassifier
-from libs.utils import load_model_from_json, fit_and_evaluate
-from libs.dataloader import load_and_split_data
+from joblib import load
 
 
 st.title("Do we need more bikes in Washington DC?")
 
-# Load random forest model from json file with optimal hyperparameters
-rf_model = load_model_from_json(RandomForestClassifier, 'output/best_params/rf_best_params.json')
+# Load the pre-trained model
+rf_model = load("output/trained_rf.joblib")
 
-# Load data
-X_train, X_test, y_train, y_test = load_and_split_data("data/training_data_preprocessed.csv", 
-                                                       target_column='increase_stock', 
-                                                       class_zero='low_bike_demand', 
-                                                       test_size=0.2, 
-                                                       random_state=0)
+# Month selector
+with st.expander("Month"):
+    months_list = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    month_str = st.radio(
+        "",
+        months_list,
+        index=datetime.date.today().month - 1,
+        horizontal=True
+    )
+    month = months_list.index(month_str) + 1
 
+# Day of week selector
+with st.expander("Day"):
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    day_str = st.radio(
+        "",
+        days,
+        horizontal=True,
+        index=datetime.date.today().weekday()+1,
+    )
+    day_of_week = days.index(day_str)
 
-def obtain_predictions(model, training_data, X_eval):
-    """
-    Obtains the predictions on the evaluation data.
-    
-    Parameters:
-        model: The model
-        training_data (csv): Path to the training data
-        X_eval (csv): Path to the evaluation data
-        target_column (str): The name of the target column to be predicted
-        class_zero (str): The name of the class to be used as the reference class (0)
-    """
-
-    # Load training data
-    training_data = pd.read_csv(training_data)
-
-    # Assign 0 to the class_zero and 1 to the other class in the target column
-    training_data['increase_stock'] = np.where(training_data['increase_stock'] == 'low_bike_demand', 0, 1)
-
-    # Split training data into features and target
-    X_train = training_data.copy()
-    y_train = X_train.pop('increase_stock')
-
-    # Load evaluation data
-    X_eval = pd.read_csv(X_eval)
-
-    # Fit the model on the training data
-    model.fit(X_train, y_train)
-
-    # Compute the predictions on the evaluation data
-    y_pred = model.predict(X_eval)
-
-    # Convert predictions to a DataFrame
-    pred_df = X_eval.copy()
-    pred_df = pred_df.drop(columns=['weekday', 'summertime', 'day'])
-    pred_df["Bike demand"] = np.where(y_pred == 0, "Low", 'High')
-    
-    return pred_df
-
-# with st.expander("Month"):
-#     this_month = datetime.date.today().month
-#     month_abbr = calendar.month_abbr[1:]
-#     month_str = st.radio("", month_abbr, index=this_month - 1, horizontal=True)
-#     month = month_abbr.index(month_str) + 1
-
-
-# Obtain predictions using the trained model
-predictions = obtain_predictions(
-    model=rf_model,
-    training_data="data/training_data_preprocessed.csv",
-    X_eval="data/test_data_preprocessed.csv"
+hour_of_day = st.slider(
+    "Hour of the day",
+    min_value=0,
+    max_value=23
 )
 
-st.dataframe(predictions, hide_index=True)
+temp = st.number_input("Temperature (ºC)")
+dew = st.number_input("Dew")
+humidity = st.number_input("Humidity")
+snowdepth = st.checkbox("Snow")
+precip = st.checkbox("Rain")
+windspeed = st.number_input("Wind speed")
+cloudcover = st.number_input("Cloud cover")
+visibility = st.number_input("Visibility")
+
+# Create a DataFrame with the input data
+input_data = pd.DataFrame({
+    "hour_of_day": [hour_of_day],
+    "day_of_week": [day_of_week],
+    "month": [month],
+    "weekday": [1 if day_of_week < 5 else 0],
+    "summertime": [1 if month in [5, 6, 7, 8] else 0],
+    "temp": [temp],
+    "dew": [dew],
+    "humidity": [humidity],
+    "precip": [1 if precip else 0],
+    "snowdepth": [1 if snowdepth else 0],
+    "windspeed": [windspeed],
+    "cloudcover": [cloudcover],
+    "visibility": [visibility]
+})
+
+
+def preprocess(df):
+    """
+    Preprocess the dataset and returns a preprocessed DataFrame.
+
+    Args:
+        df (DataFrame): DataFrame to preprocess
+
+    Returns:
+        DataFrame: Preprocessed DataFrame
+    """
+
+    # Define the numerical features
+    num_features = ['temp', 'dew', 'humidity', 'windspeed', 'cloudcover', 'visibility']
+
+    # Add a binary feature called "day" where 1 means "hour_of_day" is between 7 and 20, and 0 otherwise
+    df['day'] = ((df['hour_of_day'] >= 7) & (df['hour_of_day'] <= 20)).astype(int)
+
+    # Add a binary feature called "rain" where 1 means if "precip" is greater than 0, and 0 otherwise
+    df['rain'] = (df['precip'] > 0).astype(int)
+
+    # Drop "precip" column
+    df = df.drop(columns=['precip'])
+
+    # Normalize the numerical features
+    for feature in num_features:
+        df[feature] = (df[feature] - df[feature].mean()) / df[feature].std()
+
+    return df
+
+# Preprocess the input data
+preprocessed_df = preprocess(input_data)
+
+# Make predictions
+prediction = rf_model.predict(preprocessed_df)
+predict_proba = rf_model.predict_proba(preprocessed_df)
+
+st.write(prediction)
+st.write(predict_proba)
+
+st.subheader("Prediction")
+if prediction[0] == 0:
+    st.write("Low bike demand")
+else:
+    st.write("High bike demand")
